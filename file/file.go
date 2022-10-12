bd6167857805650381ae83d8ebb2b880dde19029
Dhamith Hewamullage <hewamullage123@gmail.com>
2022-10-13 03:00:54 +0530

    File metadata functions

--- file/file.go
+++ file/file.go
@@ -1,9 +1,54 @@
 package file
 
+import (
+	"crypto/sha256"
+	b64 "encoding/base64"
+	"io"
+	"os"
+
+	"github.com/dhamith93/aero/internal/logger"
+	"github.com/gabriel-vasile/mimetype"
+)
+
 type File struct {
 	Name string `json:"name,omitempty"`
 	Hash string `json:"hash,omitempty"`
 	Type string `json:"type,omitempty"`
 	Ext  string `json:"ext,omitempty"`
+	Path string `json:"path,omitempty"`
 	Size int64  `json:"size,omitempty"`
+}
+
+func (file *File) SetMeta() {
+	mtype, err := mimetype.DetectFile(file.Path)
+	if err != nil {
+		logger.Log("ERR", err.Error())
+		return
+	}
+	fi, err := os.Stat(file.Path)
+	if err != nil {
+		logger.Log("ERR", err.Error())
+		return
+	}
+
+	file.Ext = mtype.Extension()
+	file.Type = mtype.String()
+	file.Size = fi.Size()
+	file.Name = fi.Name()
+	file.SetHash()
+}
+
+func (file *File) SetHash() {
+	f, err := os.Open(file.Path)
+	if err != nil {
+		logger.Log("ERR", err.Error())
+		return
+	}
+	defer f.Close()
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		logger.Log("ERR", err.Error())
+		return
+	}
+	file.Hash = b64.StdEncoding.EncodeToString(h.Sum(nil))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/dhamith93/aero
 go 1.18
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	google.golang.org/grpc v1.50.0
 	google.golang.org/protobuf v1.28.1
@@ -10,8 +11,8 @@ require (
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
