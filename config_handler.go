e12840d5ac75d09a4fa401e2d6c2c1fa620e09a8
Dhamith Hewamullage <hewamullage123@gmail.com>
2022-10-13 01:07:00 +0530

    Adding config handler

---
+++ config_handler.go
@@ -0,0 +1,10 @@
+package aero
+
+type Config struct {
+	Name           string
+	Ip             string
+	Port           string
+	SocketPort     string
+	LogFileEnabled bool
+	LogFilePath    string
+}
